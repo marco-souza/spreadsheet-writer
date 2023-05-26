2b886942fd98e2bc527cc94631dde6e3649f9faf
Marco Antônio <ma.souza.junior@gmail.com>
2023-05-26 16:56:49 -0300

    enh: add get hello

--- apps/api-gateway/src/app.controller.ts
+++ apps/api-gateway/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { SpreadsheetInputDto } from '@shared/shared/interfaces/spreadsheet.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { ServicesNames, WriterEvents } from 'libs/shared/events';
@@ -6,6 +6,11 @@ import { ServicesNames, WriterEvents } from 'libs/shared/events';
 @Controller()
 export class AppController {
   constructor(@Inject(ServicesNames.WRITER) private client: ClientProxy) {}
+
+  @Get()
+  async getHello() {
+    return 'Hello from Nest.js';
+  }
 
   @Post()
   async postSpreadsheet(@Body() body: SpreadsheetInputDto) {
